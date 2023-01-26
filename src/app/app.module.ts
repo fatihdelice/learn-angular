86445d682b7be200c1195b1f4e1016c00adeb5e2
Fatih Delice <delicex58@gmail.com>
2023-01-27 00:49:44 +0300

    Product List Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProductComponent } from './product.component';
 import { SummaryPipe } from './summary.pipe';
 import { InputEmailDirective } from './input-email.directive';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
 
 
 @NgModule({
   declarations: [
     ProductComponent,
     SummaryPipe,
-    InputEmailDirective
+    InputEmailDirective,
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule
